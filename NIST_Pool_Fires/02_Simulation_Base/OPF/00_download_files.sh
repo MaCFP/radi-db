52d521b6c3b5529d31713cb882a900cc57ec271a
Fabian <braennstroem@uni-wuppertal.de>
2023-07-07 19:44:02 +0200

    NIST Pool - simulation base data initial upload

---
+++ NIST_Pool_Fires/02_Simulation_Base/OPF/00_download_files.sh
@@ -0,0 +1,37 @@
+# Select files to download from this release:
+
+# baseLink="https://github.com/MaCFP/radi-db/releases/download/FM_SimBase_v0.0.1/"
+baseLink="https://github.com/MaCFP/radi-db/releases/download/NIST_Pool_SimBase_v0.0.1/"
+
+# wget ${baseLink}01_createCaseFolder_separate.sh
+# wget ${baseLink}02_createCaseFolder_combined.sh
+
+# Constant (Mesh) and system folder
+wget ${baseLink}constant.tar.gz
+wget ${baseLink}system.tar.gz
+# wget ${baseLink}constant_system.tar
+
+
+# Time folder
+
+
+wget ${baseLink}37.tar.gz
+wget ${baseLink}37.6.tar.gz
+wget ${baseLink}37.12.tar.gz
+wget ${baseLink}37.24.tar.gz
+wget ${baseLink}37.36.tar.gz
+wget ${baseLink}37.48.tar.gz
+wget ${baseLink}37.72.tar.gz
+wget ${baseLink}37.84.tar.gz
+wget ${baseLink}37.96.tar.gz
+wget ${baseLink}38.2.tar.gz
+wget ${baseLink}38.08.tar.gz
+wget ${baseLink}38.8.tar.gz
+wget ${baseLink}38.32.tar.gz
+wget ${baseLink}38.44.tar.gz
+wget ${baseLink}38.56.tar.gz
+wget ${baseLink}38.68.tar.gz
+wget ${baseLink}38.92.tar.gz
+wget ${baseLink}39.04.tar.gz
+wget ${baseLink}39.16.tar.gz
+wget ${baseLink}39.28.tar.gz
