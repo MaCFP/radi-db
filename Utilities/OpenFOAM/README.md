84121d69f6606c5e1a4d4ccd79954476fa53925e
Fabian <braennstroem@uni-wuppertal.de>
2023-09-07 13:07:29 +0200

    Utilities - plot python and adjusted fireFoam

---
+++ Utilities/OpenFOAM/README.md
@@ -0,0 +1,5 @@
+# fireFoam adjusted for pure radiation
+- Focus on pure radiation modeling; no momentum or energy equation solved
+- Tested with OpenFOAM v2212
+- Needed for running openfoam simulations and for post-processing of PMC-LBL results
+
---
+++ Utilities/OpenFOAM/fireRADFoam_MaCFP/fireRADFoam_MaCFP.C
@@ -0,0 +1,112 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2017 OpenFOAM Foundation
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    fireFoam
+
+Group
+    grpCombustionSolvers
+
+Description
+    Transient solver for fires and turbulent diffusion flames with reacting
+    particle clouds, surface film and pyrolysis modelling.
+
+    Adjustments for MaCFP Radiation Subgroup (FB 7.9.2023)
+    - focus on pure radiation modeling
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "turbulentFluidThermoModel.H"
+#include "basicReactingCloud.H"
+#include "surfaceFilmModel.H"
+#include "pyrolysisModelCollection.H"
+#include "radiationModel.H"
+#include "SLGThermo.H"
+#include "solidChemistryModel.H"
+#include "psiReactionThermo.H"
+#include "CombustionModel.H"
+#include "pimpleControl.H"
+#include "fvOptions.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::addNote
+    (
+        "Transient solver for fires and turbulent diffusion flames"
+        " with reacting particle clouds, surface film and pyrolysis modelling."
+    );
+
+    #include "postProcess.H"
+
+    #include "addCheckCaseOptions.H"
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createControl.H"
+    #include "createFields.H"
+    #include "createFieldRefs.H"
+    #include "initContinuityErrs.H"
+    #include "createTimeControls.H"
+    #include "compressibleCourantNo.H"
+    #include "setInitialDeltaT.H"
+    /* #include "createRegionControls.H" */
+
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
+    {
+        #include "readTimeControls.H"
+        #include "setDeltaT.H"
+
+        ++runTime;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+
+            // --- PIMPLE loop
+            while (pimple.loop())
+            {
+                #include "YEEqn.H"
+
+            }
+
+        runTime.write();
+
+        runTime.printExecutionTime(Info);
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
