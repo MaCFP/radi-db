5d6ee78f140e91cd65a396f12f279f8adea1fdf2
Fabian <braennstroem@uni-wuppertal.de>
2023-09-07 10:00:45 +0200

    FM Burner - PMC_LBL scripts

--- FM_Burner/03_Simulation_LBL_PMC/00_download_files.sh
+++ FM_Burner/03_Simulation_LBL_PMC/00_download_files.sh
@@ -1,53 +1,51 @@
 # Select files to download from this release:
 baseLink="https://github.com/MaCFP/radi-db/releases/download/FM_LBL_PMC_v0.0.1/"
 
+cd Release
 
-# Constant (Mesh) and system folder
-wget ${baseLink}constant.tar.gz
-wget ${baseLink}system.tar.gz
-
-
+# Constant (Mesh) 
+wget ${baseLink}constant_polyMesh.tar.gz
 wget ${baseLink}SpectralRad.tar.gz
 
 
 # Time folder
-wget ${baseLink}5.00009.tar
-wget ${baseLink}10.0001.tar
-wget ${baseLink}15.0004.tar
-wget ${baseLink}20.0003.tar
-wget ${baseLink}25.0001.tar
-wget ${baseLink}30.0002.tar
-wget ${baseLink}35.0002.tar
-wget ${baseLink}40.0001.tar
-wget ${baseLink}45.0004.tar
-wget ${baseLink}50.0005.tar
-wget ${baseLink}55.0001.tar
-wget ${baseLink}60.0005.tar
-wget ${baseLink}65.0001.tar
-wget ${baseLink}70.0004.tar
-wget ${baseLink}75.0002.tar
-wget ${baseLink}80.0003.tar
-wget ${baseLink}85.tar     
-wget ${baseLink}90.0005.tar
-wget ${baseLink}95.0002.tar
-wget ${baseLink}100.tar    
-wget ${baseLink}105.tar    
-wget ${baseLink}110.tar    
-wget ${baseLink}115.001.tar
-wget ${baseLink}120.tar    
-wget ${baseLink}125.tar    
-wget ${baseLink}130.tar    
-wget ${baseLink}135.tar    
-wget ${baseLink}140.tar    
-wget ${baseLink}145.tar    
-wget ${baseLink}150.tar    
-wget ${baseLink}155.tar    
-wget ${baseLink}160.tar    
-wget ${baseLink}165.tar    
-wget ${baseLink}170.tar    
-wget ${baseLink}175.tar    
-wget ${baseLink}180.tar    
-wget ${baseLink}185.tar    
-wget ${baseLink}190.tar    
-wget ${baseLink}195.tar    
-wget ${baseLink}200.tar    
+wget ${baseLink}5.00009.tar.gz
+wget ${baseLink}10.0001.tar.gz
+wget ${baseLink}15.0004.tar.gz
+wget ${baseLink}20.0003.tar.gz
+wget ${baseLink}25.0001.tar.gz
+wget ${baseLink}30.0002.tar.gz
+wget ${baseLink}35.0002.tar.gz
+wget ${baseLink}40.0001.tar.gz
+wget ${baseLink}45.0004.tar.gz
+wget ${baseLink}50.0005.tar.gz
+wget ${baseLink}55.0001.tar.gz
+wget ${baseLink}60.0005.tar.gz
+wget ${baseLink}65.0001.tar.gz
+wget ${baseLink}70.0004.tar.gz
+wget ${baseLink}75.0002.tar.gz
+wget ${baseLink}80.0003.tar.gz
+wget ${baseLink}85.tar.gz     
+wget ${baseLink}90.0005.tar.gz
+wget ${baseLink}95.0002.tar.gz
+wget ${baseLink}100.tar.gz    
+wget ${baseLink}105.tar.gz    
+wget ${baseLink}110.tar.gz    
+wget ${baseLink}115.001.tar.gz
+wget ${baseLink}120.tar.gz    
+wget ${baseLink}125.tar.gz    
+wget ${baseLink}130.tar.gz    
+wget ${baseLink}135.tar.gz    
+wget ${baseLink}140.tar.gz    
+wget ${baseLink}145.tar.gz    
+wget ${baseLink}150.tar.gz    
+wget ${baseLink}155.tar.gz    
+wget ${baseLink}160.tar.gz    
+wget ${baseLink}165.tar.gz    
+wget ${baseLink}170.tar.gz    
+wget ${baseLink}175.tar.gz    
+wget ${baseLink}180.tar.gz    
+wget ${baseLink}185.tar.gz    
+wget ${baseLink}190.tar.gz    
+wget ${baseLink}195.tar.gz    
+wget ${baseLink}200.tar.gz    
